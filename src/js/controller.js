188063731961300cd494b5f6ef8484572e5f6a76
carloszaep <106843133+carloszaep@users.noreply.github.com>
2022-09-27 18:11:06 -0400

    add npm and refactoring for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,8 @@
+import * as model from './module.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +16,24 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+    // LOADING RECIPE
+    await model.loadRecipe(id);
+
+    // RENDER RECIPE
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+['hashchange', 'load'].forEach(e => window.addEventListener(e, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
